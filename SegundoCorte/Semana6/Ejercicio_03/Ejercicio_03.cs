70b91523426199f0915304353e97acb5d43047b8
Maripau11 <mtobon@ucompensar.edu.co>
2022-04-21 00:23:48 -0500

    Semana6

---
+++ SegundoCorte/Semana6/Ejercicio_03/Ejercicio_03.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace Ejercicio_03
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Este ejercicio no se debe realizar");
+            Console.ReadKey();
+        }
+    }
+}
